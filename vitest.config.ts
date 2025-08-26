651bfba89a70437eb4a2e7403622f4f4a641a214
Thorn Walli <lammpee@gmail.com>
2025-08-26 23:08:08 +0200

    chore(vitest): remove unused project configurations

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,19 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    projects: [
-      {
-        extends: './packages/core/vitest.config.ts',
-        test: {
-          name: 'core'
-        }
-      },
-      {
-        extends: './packages/disk-synthesizer/vitest.config.ts',
-        test: {
-          name: 'disk-synthesizer'
-        }
-      }
-    ]
+    projects: []
   }
 });
